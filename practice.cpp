e65522f3633ca40126209747a5759b79b3895798
MinMin <minhnguyen2587@gmail.com>
2021-07-28 11:06:31 +0700

    Duplicated string by Minh

--- practice.cpp
+++ practice.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 // not using string
-void duplicateStr(char str[])
-{
-    cout << "duplicated: " << str << endl;
+void duplicateStr(char str[]){
+	int n=0,i=0;
+	char s[20];
+	while (str[i] != '\0'){
+		++n;
+		s[i] = str[i];
+		++i;
+	}
+	for (i=0;i<n;i++){
+		s[i+n] = str[i];
+	}
+    cout << "duplicated: " << s << endl;
 }
 
 void reverseStr(char str[])
