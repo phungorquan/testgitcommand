3b5d216ea3f07e6902273afe0d97d3698593ab2c
nguyenhoangtan4409 <75473818+nguyenhoangtan4409@users.noreply.github.com>
2021-08-02 22:14:41 +0700

    Test merge (#3)

--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int getStrLen(char str[]){
+// tada
+int getStrLen(char str[]) {
     int len = 0;
     while (str[len] != '\0')
     {
@@ -15,11 +16,11 @@ void duplicateStr(char str[])
 {
     unsigned int n = 2;
     unsigned int len = getStrLen(str);
-    unsigned char dupStr[len*n];
+    unsigned char dupStr[len * n];
     unsigned int strIndex = 0, dupStrIndex = 0;
     for (int i = 1; i <= n; i++)
     {
-    	unsigned int maxStrLen = len*i;
+        unsigned int maxStrLen = len * i;
         for (dupStrIndex; dupStrIndex < maxStrLen; dupStrIndex++)
         {
             dupStr[dupStrIndex] = str[strIndex];
@@ -28,7 +29,7 @@ void duplicateStr(char str[])
         dupStrIndex = maxStrLen;
         strIndex = 0;
     }
-    
+
     cout << "duplicated: " << dupStr << endl;
 }
 
@@ -37,20 +38,20 @@ void reverseStr(char str[])
     unsigned int len = getStrLen(str);
     unsigned char reverseStr[len];
     unsigned int indexReverseStr = len - 1;
-    
-    for (int i = 0 ; i < len; i++)
+
+    for (int i = 0; i < len; i++)
     {
         reverseStr[i] = str[indexReverseStr];
         indexReverseStr--;
     }
-    
+
     cout << "reversed: " << reverseStr << endl;
 }
 
 
-int main(){
-	char str[] = "abcdefg";
- 	duplicateStr(str);
- 	reverseStr(str);
-	return 0;
+int main() {
+    char str[] = "abcdefg";
+    duplicateStr(str);
+    reverseStr(str);
+    return 0;
 }
