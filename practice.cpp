335970757c9200a681f7d5a53e338586acddaf2f
phungorquan <anhquantong77@gmail.com>
2021-08-02 19:27:29 +0700

    Handle duplicate and reverse

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
 using namespace std;
 
+int getStrLen(char str[]){
+    int len = 0;
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+    return len;
+}
+
 // not using string
 void duplicateStr(char str[])
 {
-    cout << "duplicated: " << str << endl;
+    unsigned int n = 2;
+    unsigned int len = getStrLen(str);
+    unsigned char dupStr[len*n];
+    unsigned int strIndex = 0, dupStrIndex = 0;
+    for (int i = 1; i <= n; i++)
+    {
+    	unsigned int maxStrLen = len*i;
+        for (dupStrIndex; dupStrIndex < maxStrLen; dupStrIndex++)
+        {
+            dupStr[dupStrIndex] = str[strIndex];
+            strIndex++;
+        }
+        dupStrIndex = maxStrLen;
+        strIndex = 0;
+    }
+    
+    cout << "duplicated: " << dupStr << endl;
 }
 
 void reverseStr(char str[])
 {
-    cout << "reversed: " << str << endl;
+    unsigned int len = getStrLen(str);
+    unsigned char reverseStr[len];
+    unsigned int indexReverseStr = len - 1;
+    
+    for (int i = 0 ; i < len; i++)
+    {
+        reverseStr[i] = str[indexReverseStr];
+        indexReverseStr--;
+    }
+    
+    cout << "reversed: " << reverseStr << endl;
 }
 
 
